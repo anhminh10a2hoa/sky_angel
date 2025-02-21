2a530237f1071b8b08a0ba971527489ff614620c
Minh Hoang <anhminh10a2hoa@gmail.com>
2025-02-22 00:00:30 +0200

    Add GitHub Action for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import eslint from 'vite-plugin-eslint';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), eslint()],
+  base: '/sky-angel/',
   test: {
     globals: true,
     environment: 'jsdom',
